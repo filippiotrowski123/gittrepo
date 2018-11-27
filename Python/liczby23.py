a2837a458789ab6e1e0662fe200aec139b029cb2
filippiotrowski123 <filip.piotrowski@lo1.sandomierz.pl>
2018-11-27 13:32:36 +0100

    funkcie listy zbioru

---
+++ Python/liczby23.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  liczby2 3py
+#  
+def liczby2():
+    """
+    Drukowanie wszystkich cyfr 2-cyfrowsych 
+    liczby o niepowtarzajacych się cyfrach
+     nie drukujemy:11, 22, 33, ..."""
+    licznik = 0
+    for i in range(1, 10):
+        for j in range(0, 10):
+            if i != j:
+                print("{}{}".format(i, j))
+                licznik = licznik + 1
+    print("liczb 2 syfrowych",licznik)
+
+def main(args):
+    liczby2()
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
---
+++ Python/lotek.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  lotek.py
+import random
+
+def main(args):
+    ileliczb = 3
+    liczby = []
+    #for i in range(3):
+    
+    while ileliczb:
+        liczba = random.randint(0, 10)
+        if not licby.count(liczby):
+            liczby.append(liczba)
+            ileliczb -= 1
+    print(liczby)
+    
+    
+    ileliczb = len(liczby)
+    type = set()
+    while ileliczb:
+        typ = int(input("Podaj typ:"))
+        
+        if typ not in typy:
+            typy.add(typy)
+            ileliczb = 1
+    print:(typy)
+    trefione = set(liczby) & typy
+    
+    if tarafione:
+        print("Trefione", trafione)
+    
+    else:
+        print("spróbuj jeszcze raz!")
+    
+    print(typy)
+def main(args):
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
