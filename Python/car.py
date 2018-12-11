e385a6ecdaa914b21e1692d95c667b71af70048e
filippiotrowski123 <filip.piotrowski@lo1.sandomierz.pl>
2018-12-11 13:30:46 +0100

    car

--- Python/car.py
+++ Python/car.py
@@ -2,26 +2,64 @@
 # -*- coding: utf-8 -*-
 #
 #  car.py
-# marka np fiat
-# model np tipo
-# rok produkcji np 2002
-# metoda wiek(), która zwróci wiek samochodu w latch
-from datetime import date
 
-class samochód:
+# zdefiniuj klasę Samochod z nast. atrybutami i metodami:
+# marka, np. Fiat
+# model, np. Tipo
+# rok produkcji, np. 2002
+# metoda wiek(), która zwróci wiek samochodu w latach
+from datetime import date
+from osoba_obj import Osoba
+
+class Kierowca(Osoba):
+    
+    licznik = 0 #atrybut klasy, współdzielony przez obiekty
+    
+    def __init__(self, imie, nazwisko, kategoria):
+        super().__init__(imie, nazwisko)
+        self.kategoria = kategoria
+        Kierowca.__licznik += 1
+    
+    def _del_(self):
+        Kierowca._licznik -= 1
+    @staticmethod # metoda statyczna
+    def KierowcaLicz():
+        return Kierowca.__licznik
+
+# todo ....
+
+class Samochod:
+    
     def __init__(self, marka, model, rocznik):
-        self.maeka = marka
+        self.marka = marka
         self.model = model
         self.rok = rocznik
+        self.Kierowca = Kierowca
+
     def wiek(self):
         dzis = date.today()
         return dzis.year - self.rok
-        
 
 def main(args):
-    s1 = Samochod('Fita', 'Tipo', 2000)
-    s2 = Samochod('Peugot', '307', 2007)
-    s3 = Samochod('Opel', 'Astra', 2003)
+    s1 = Samochod('Fiat', 'Tipo', 2000)
+    s2 = Samochod('Peugeot', '308', 2007)
+    print(s1.model, s1.marka, s1.wiek())
+    print(s2.model, s1.marka, s1.wiek())
+    
+    k1 = Kierowca('Adam', 'Słodowy', 'B')
+    print(k1.imie, k1.kategoria)
+    print(k1.KierowcaLicz())
+    k2 = Kierowca('Ewa', 'Bąk', 'BC')
+    
+    k3 = Kierowca('Bartek', 'Dąb', 'BC')
+    print(k1.KierowcaLicz())
+    print(k2.KierowcaLicz())
+    print(k3.KierowcaLicz())
+    
+    del(k2)
+    del(k3)
+    print(k1.KierowcaLicz())
+    
     return 0
 
 if __name__ == '__main__':
--- Python/osoba_obj.py
+++ Python/osoba_obj.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  osoba_obi.py
+#  osoba_obj.py
 
-class osoba:
-    """Prosta klasa opisująca osobę"""
-    def __init__(self, imie, nazwisko):
+class Osoba:
+    """ Prosta klasa opisująca osobę """
+
+    def __init__(self, imie, nazwisko, wiek=0):
         self.imie = imie
         self.nazwisko = nazwisko
         self.ustawPlec()
         self.wiek = wiek
-        
-    
+
     def ustawPlec(self):
         if self.imie[-1] == 'a':
             self.plec = 'k'
         else:
             self.plec = 'm'
-        
+
 def main(args):
-    o1 = osoba('Adam', 'Słodowy')
+    o1 = Osoba('Adam', 'Słodowy')
     o1.wiek = 23
-    o2 = osoba('Ewa', 'Kos')
+    o2 = Osoba('Ewa', 'Kos')
     o2.wiek = 20
     
-    print(o1.imie, o1.nazwisko, o1.plec, o1.wiek)
-    print(o2.imie, o2.nazwisko, o2.plec, o2.wiek)
-    
+    print(o1.imie, o1.nazwisko, o1.plec)
+    print(o2.imie, o2.nazwisko, o2.plec)
+
     return 0
 
 if __name__ == '__main__':
