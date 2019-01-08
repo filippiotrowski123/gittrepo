59ad1e62412a18c49b4bcc96849761c151d59dad
filippiotrowski123 <filip.piotrowski@lo1.sandomierz.pl>
2019-01-08 13:31:17 +0100

    silnia

--- Python/euklides.py
+++ Python/euklides.py
@@ -3,7 +3,7 @@
 #
 #  euklides.py
 def nwdv1(a, b):
-    while not a == b:
+    while a != b:
         if a > b:
             a = a - b
         else:
@@ -18,13 +18,18 @@ def nwdv2(a, b):
         
             
     return b
-
+def testuj():
+    assert(nwdv1(20, 4) == 4)
+    assert(nwdv2(20, 4) == 4)
+    assert(nwdv2(11, 4) == 1)
 
 
 def main(args):
     a = int(input('Podaj liczbe a:'))
     b = int(input('Podaj liczbę b:'))
-    print(nwdv2(a, b))
+    #nwdv1(a, b)
+    #print(nwdv2(a, b))
+    testuj()
     return 0
 
 if __name__ == '__main__':
