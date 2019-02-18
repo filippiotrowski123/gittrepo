b085718470ba84fcb3f5d083a875f61203e1a47a
filippiotrowski123 <filip.piotrowski@lo1.sandomierz.pl>
2019-02-18 12:36:29 +0100

    funkcaj_rek

--- Python/silnia.py
+++ Python/silnia.py
@@ -12,15 +12,24 @@ def silnia_it(n):
     for i in range(1, n + 1):
         wynik = wynik * i
     return wynik
-def main(args):4
-    n = int(input('Podaj liczbe naturalną:'))
-    wynik = 1
-    for i in range(1, n + 1):
-        wynik = wynik * i
-    print(wynik)
-    
-    return 0
 
+def silnia_rek(n):
+    #n! =(n-1)! * n
+    if n == 0:
+        return 1
+    return silnia_rek(n-1) * n
+
+
+
+def main(args):
+    assert silnia_rek(0) == 1
+    assert silnia_rek(1) == 1
+    assert silnia_rek(2) == 2
+    assert silnia_rek(3) == 6
+
+    n = int(input("Podaj liczbę naturalną: "))
+    print("{}! = {}".format(n, silnia_it(n)))
+    return 0
 if __name__ == '__main__':
     import sys
     sys.exit(main(sys.argv))
