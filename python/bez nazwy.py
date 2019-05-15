1064d34975c5ddb1c625ad24119046c96b90750e
filippiotrowski123 <filip.piotrowski@lo1.sandomierz.pl>
2019-05-15 09:18:35 +0200

    html

---
+++ python/bez nazwy.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  bez nazwy.py
+
+
+def main(args):
+    a=0
+    a=1
+    print(a)
+        
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
