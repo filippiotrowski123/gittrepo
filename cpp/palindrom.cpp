2fe541d9f3fb3c8672f9101c4de1da23ad22299f
filippiotrowski123 <filip.piotrowski@lo1.sandomierz.pl>
2019-11-15 09:38:50 +0100

    1

--- cpp/palindrom.cpp
+++ cpp/palindrom.cpp
@@ -2,13 +2,61 @@
  * palindrom.cpp
 
  */
-
-
 #include <iostream>
+#include <cstring>
+
 using namespace std;
+
+
+
+bool czy_palindrom(char tab[]){
+	int rozmiar = strlen(tab) - 1;
+	for(int i = 0; i < rozmiar/2; i++){
+		if(tab[i] == tab[rozmiar-1 - i])
+			;
+		else
+			return false;
+			}
+		
+		return true;
+	}
+
+void usunSpace(char tab1[], char tab2[]){
+	int rozmiar = strlen(tab1);
+	for(int i = 0; i < rozmiar; i++){
+		if(tab1[i] != ' '){
+			tab2[i] = tab1[i];
+			}
+		
+	}
+}
+bool czy_palindrom2(char tab[]){
+	int rozmiar = strlen(tab);
+	for(int i = 0; i < rozmiar/2; i++){
+		if(tab[i] == ' ') i++;
+		if(tab[i] == tab[rozmiar- 1 - i])
+			;
+		else
+			return false;
+			}
+		
+		return true;
+	}
 int main(int argc, char **argv)
 {
-	
+	int r = 20;
+	char napis1[r];
+	char napis2[r];
+	cin.getline(napis1, 20);
+	usunSpace(napis1, napis2);
+	cout << napis2 << endl;
+	cout << strlen(napis2) << endl;
+	if(czy_palindrom(napis2))
+		cout << "To palindrom!" << endl;
+		else{
+			cout << "To nie palindrom" << endl;
+	}
 	return 0;
 }
+
 
