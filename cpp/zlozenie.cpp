bc6990e3826c6753d90692450569ff7e352cc3f4
filippiotrowski123 <filip.piotrowski@lo1.sandomierz.pl>
2020-01-15 09:41:15 +0100

    23

---
+++ cpp/zlozenie.cpp
@@ -0,0 +1,19 @@
+/*
+ * zlozenie.cpp
+ */
+#include <iostream>
+using namespace std;
+int main(int argc, char **argv)
+{
+    int n;
+    int i = 1;
+ 	cout << "Podaj n" << endl;
+    cin >> n;
+    while (i <= n) { 
+        cout << i << " ";
+        i+=2;
+}
+
+	return 0;
+}
+
